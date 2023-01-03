f0104a880fcc64de4e3e4a9677b91234861d4f9b
Madhusudan239 <120104459+Madhusudan239@users.noreply.github.com>
2023-01-03 16:33:25 +0530

    Update lambda_validation_oldversion_12262022.py

--- lambda_validation_oldversion_12262022.py
+++ lambda_validation_oldversion_12262022.py
@@ -163,4 +163,4 @@ def lambda_handler(event, context):
         o= PreprocessValidation()
         c,d = o.validate(bucket_name, preprocess_folder_name, bucket_name, human_review_folder_name)
         print('madhu')
-        print('madhu11)
+        print('madhu1')
